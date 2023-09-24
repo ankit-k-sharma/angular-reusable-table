88897eeda1948f55eba6b4b8cf98b1965e44e1cc
Ankit Kumar Sharma <aks.computerexpert@gmail.com>
2023-09-24 21:15:31 +0530

    update angular version from 11 to 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { OtherComponent } from './other/other.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
   { path: 'home', component: HomeComponent },
   { path: 'customer', component: CustomerComponent },
   { path: 'other', component: OtherComponent },
